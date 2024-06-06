331c9f237c363229893b7fdc2561118d0b7a45b3
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-06-06 18:46:06 +0300

    proje tamamlandı

--- assets/js/cars.js
+++ assets/js/cars.js
@@ -1,152 +1,111 @@
-let cardnumber = document.querySelector(".card-numbers");
-let cardname = document.querySelector(".name");
-let monthyear = document.querySelector(".month-year");
-let cvc = document.querySelector(".cvc");
-let cardnumberinput = document.querySelector(".card-number-input");
-let cardnameinput = document.querySelector(".card-name-input");
-let ayinput = document.querySelector(".ay-input");
-let yilinput = document.querySelector(".yil-input");
-let cvcinput = document.querySelector(".cvc-input");
-let thanks = document.querySelector(".thanks");
-let button = document.querySelector(".submit");
-let devam = document.querySelector(".devam");
-let cardetails = document.querySelector(".card-details");
-let uyaricardname = document.querySelector(".uyari-cardname");
-let uyaricardnumber = document.querySelector(".uyari-cardnumber");
-let uyaricardaayy = document.querySelector(".uyari-cardaayy");
-let uyaricardcvc = document.querySelector(".uyari-cardcvc");
+let cars = [
+  { 
+      brand: "Ford", 
+      model: "Mustang", 
+      color: "Mavi" 
+  },
+  { 
+      brand: "BMW", 
+      model: "3 Serisi", 
+      color: "Siyah" 
+  },
+  { 
+      brand: "Mercedes-Benz", 
+      model: "E Serisi", 
+      color: "Kırmızı" 
+  },
+  { 
+      brand: "Toyota", 
+      model: "Corolla", 
+      color: "Beyaz" 
+  },
+  { 
+      brand: "Honda", 
+      model: "Civic", 
+      color: "Gri" 
+  },
+  { 
+      brand: "Volkswagen", 
+      model: "Golf", 
+      color: "Yeşil" 
+  },
+  { 
+      brand: "Audi", 
+      model: "A4", 
+      color: "Turuncu" 
+  },
+  { 
+      brand: "Porsche", 
+      model: "911", 
+      color: "Sarı" 
+  },
+  { 
+      brand: "Ferrari", 
+      model: "488 GTB", 
+      color: "Lacivert" 
+  },
+  { 
+      brand: "Lamborghini", 
+      model: "Aventador", 
+      color: "Altın" 
+  },
+  { 
+      brand: "Tesla", 
+      model: "Model S", 
+      color: "Gümüş" 
+  },
+  { 
+      brand: "Subaru", 
+      model: "Impreza", 
+      color: "Mor" 
+  },
+  { 
+      brand: "Chevrolet", 
+      model: "Camaro", 
+      color: "Turkuaz" 
+  },
+  { 
+      brand: "Hyundai", 
+      model: "Tucson", 
+      color: "Kahverengi" 
+  },
+  { 
+      brand: "Kia", 
+      model: "Sportage", 
+      color: "Pembe" 
+  },
+  { 
+      brand: "Nissan", 
+      model: "Altima", 
+      color: "Beyaz" 
+  },
+  { 
+      brand: "Jaguar", 
+      model: "F-Type", 
+      color: "Siyah" 
+  },
+  { 
+      brand: "Land Rover", 
+      model: "Range Rover", 
+      color: "Gri" 
+  },
+  { 
+      brand: "Volvo", 
+      model: "XC90", 
+      color: "Yeşil" 
+  },
+  { 
+      brand: "Mazda", 
+      model: "MX-5", 
+      color: "Kırmızı" 
+  }
+];
+let brand = document.querySelectorAll(".brand");
+let model = document.querySelectorAll(".model");
+let color = document.querySelectorAll(".color");
 
-function handleİnputClick(){
-  cardname.innerText = cardnameinput.value;
-  cardnumber.innerText = cardnumberinput.value;
-  cvc.innerText = cvcinput.value;
-  monthyear.innerText =ayinput.value + "/" + yilinput.value;
+for (let i=0 ; i< 20; i++) {
+  brand[i].innerText += cars[i].brand;
+  model[i].innerText += cars[i].model;
+  color[i].innerText += cars[i].color;
 }
-cardnameinput.oninput = function() {handleİnputClick()};
-cardnumberinput.oninput = function() {handleİnputClick()};
-cvcinput.oninput = function() {handleİnputClick()};
-yilinput.oninput = function() {handleİnputClick()};
-ayinput.oninput = function() {handleİnputClick()};
-
-function handleSubmitClick (e) {
-    e.preventDefault();
-  if (
-    (cardnameinput.value !== "",
-    cardnumberinput.value !== "",
-    cvcinput.value !== "",
-    ayinput.value !== "",
-    yilinput.value !== "")
-  ) {
-    cardname.innerText = cardnameinput.value;
-    cardnumber.innerText = cardnumberinput.value;
-    cvc.innerText = cvcinput.value;
-    monthyear.innerText =ayinput.value + "/" + yilinput.value;
-    cardetails.style.display = "none";
-    thanks.style.display = "flex";
-  }
-  
-  if (cardnameinput.value === "") {
-    uyaricardname.style.display = "flex";
-    cardnameinput.style.border = "1px solid rgba(215, 60, 60, 1)";
-  } else {
-    uyaricardname.innerText = "";
-    cardnameinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  }
-
-
-
-  if (cardnumberinput.value === "") {
-    uyaricardnumber.style.display = "flex";
-    cardnumberinput.style.border = "1px solid rgba(215, 60, 60, 1)";
-  } else {
-    uyaricardnumber.innerText = "";
-    cardnumberinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  }
-
-
-
-  if (yilinput.value === "", ayinput.value === "") {
-    uyaricardaayy.style.display = "flex";
-    yilinput.style.border = "1px solid rgba(215, 60, 60, 1)";
-    ayinput.style.border = "1px solid rgba(215, 60, 60, 1)";
-  } else {
-    uyaricardaayy.innerText = "";
-    yilinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-    ayinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  }
-
-  if (cvcinput.value === "") {
-    uyaricardcvc.style.display = "flex";
-    cvcinput.style.border = "1px solid rgba(215, 60, 60, 1)";
-  } else {
-    uyaricardcvc.innerText = "";
-    cvcinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  }
-
-  cardetails.reset();
-}
-
-button.addEventListener("click",handleSubmitClick);
-
-function handleContinueClick(){
-  
-  cardname.innerText = "Muhammed Baki Çağlayan";
-  cardnameinput.value = "";
-  cardnumber.innerText = "0000 0000 0000 0000";
-  cardnumberinput.value = "";
-  cvc.innerText  = "000";
-  cvcinput.value = "";
-  monthyear.innerText  = "00/00";
-  ayinput.value = "";
-  yilinput.value = "";
-  cardetails.style.display = "flex";
-  thanks.style.display = "none";
-  cardnameinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  cardnumberinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  yilinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  ayinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  
-  if (cardnameinput.value === "") {
-    uyaricardname.style.display = "flex";
-    cardnameinput.style.border = "1px solid rgba(215, 60, 60, 1)";
-  } else {
-    uyaricardname.innerText = "";
-    cardnameinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  }
-
-  if (cardnumberinput.value === "") {
-    uyaricardnumber.style.display = "flex";
-    cardnumberinput.style.border = "1px solid rgba(215, 60, 60, 1)";
-  } else {
-    uyaricardnumber.innerText = "";
-    cardnumberinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  }
-
-  if (yilinput.value === "", ayinput.value === "") {
-    uyaricardaayy.style.display = "flex";
-    yilinput.style.border = "1px solid rgba(215, 60, 60, 1)";
-    ayinput.style.border = "1px solid rgba(215, 60, 60, 1)";
-  } else {
-    uyaricardaayy.innerText = "";
-    yilinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-    ayinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  }
-
-  if (cvcinput.value === "") {
-    uyaricardcvc.style.display = "flex";
-    cvcinput.style.border = "1px solid rgba(215, 60, 60, 1)";
-  } else {
-    uyaricardcvc.innerText = "";
-    cvcinput.style.border = "1px solid rgba(223, 222, 224, 1)";
-  }
-}
-
-devam.addEventListener("click",handleContinueClick);
-
-function maxlength(event) {
-  let t = event.target;
-  if (t.hasAttribute('maxlength')) {
-    t.value = t.value.slice(0, t.getAttribute('maxlength'));
-  }
-}
-document.body.addEventListener('input', maxlength);
